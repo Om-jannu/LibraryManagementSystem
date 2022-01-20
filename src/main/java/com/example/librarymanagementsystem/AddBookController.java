d5e56d102643485e3866434074820bb2aa3ef12a
Omjannu <89771105+Om-jannu@users.noreply.github.com>
2022-01-20 22:20:57 +0530

    updated addbookcontrol

--- src/main/java/com/example/librarymanagementsystem/AddBookController.java
+++ src/main/java/com/example/librarymanagementsystem/AddBookController.java
@@ -11,8 +11,6 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
